955b1d73c1adb3f07c362f7659b3dccc690cf8ad
Lê Quang Ánh <superanhziggy@gmail.com>
2024-11-19 23:43:08 +0900

    area

---
+++ RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RectangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter width: ");
+            float width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            float height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+        }
+    }
+}
