69ecd50eb6fc4182ff5271204a8c63e9982fdcac
Lê Quang Ánh <superanhziggy@gmail.com>
2024-12-03 21:11:49 +0700

    stop watch

---
+++ StopWatchOOP/StopWatchOOP/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace StopWatchOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+
+            // Tạo mảng 100,000 số ngẫu nhiên
+            Random rand = new Random();
+            int[] numbers = new int[100000];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rand.Next(1, 1000000);
+            }
+
+            // Bắt đầu đo thời gian
+            stopWatch.Start();
+
+            // Thực hiện thuật toán Selection Sort
+            SelectionSort(numbers);
+
+            // Dừng đo thời gian
+            stopWatch.Stop();
+
+            // Hiển thị thời gian đã trôi qua
+            Console.WriteLine("Time elapsed for Selection Sort: " + stopWatch.GetElapsedTime() + " milliseconds");
+        }
+
+        // Phương thức Selection Sort
+        static void SelectionSort(int[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (array[j] < array[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                // Swap
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
+}
---
+++ StopWatchOOP/StopWatchOOP/StopWatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatchOOP
+{
+    internal class StopWatch
+    {
+        
+        private DateTime startTime;
+        private DateTime endTime;
+
+        
+        public DateTime StartTime => startTime;
+        public DateTime EndTime => endTime;
+
+        
+        public StopWatch()
+        {
+            
+            startTime = DateTime.Now;
+        }
+
+        
+        public void Start()
+        {
+            startTime = DateTime.Now;
+        }
+
+        
+        public void Stop()
+        {
+            endTime = DateTime.Now;
+        }
+
+        
+        public double GetElapsedTime()
+        {
+            return (endTime - startTime).TotalMilliseconds;
+        }
+    }
+}
